d0f3d0ee82706f88134557e26cab35883231e82c
Rohan Pandit <145998760+rohan-nerdster@users.noreply.github.com>
2025-01-13 15:05:01 +0000

    Change branding to Blisss & Lab Technologies

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = 'Blisss Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_NAME = 'Blisss';
+export const BRANDING_LOGO_URL = 'images/logo.png';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'Lab Technologies';
 
 export const BRANDING_URL = {
   help: undefined,
